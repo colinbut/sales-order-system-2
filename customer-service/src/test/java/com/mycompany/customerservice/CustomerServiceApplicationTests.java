95bcec715b8ea6f54e60c66118c271ec6b93077b
Colin But <colin.but@outlook.com>
2020-03-16 22:19:41 +0000

    Added customer-service skeleton

---
+++ customer-service/src/test/java/com/mycompany/customerservice/CustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycompany.customerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
