57d87614340e49610d181f4540d15335f6c839b4
Colin But <colin.but@outlook.com>
2020-03-22 14:36:53 +0000

    add missing @Repository annotation on ItemRepository

--- product-service/src/main/java/com/mycompany/productservice/repository/ItemRepository.java
+++ product-service/src/main/java/com/mycompany/productservice/repository/ItemRepository.java
@@ -2,6 +2,8 @@ package com.mycompany.productservice.repository;
 
 import com.mycompany.productservice.entity.Item;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ItemRepository extends CrudRepository<Item, String> {
 }
