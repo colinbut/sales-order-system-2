5e640dfe782c5864f2cf640f853828b3496f74b4
Colin But <colin.but@outlook.com>
2020-03-25 14:42:52 +0000

    setting productservice up for integration test

---
+++ product-service/src/test/java/com/mycompany/productservice/ApiProductIntegrationTest.java
@@ -0,0 +1,11 @@
+package com.mycompany.productservice;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+public class ApiProductIntegrationTest {
+
+
+}
