727d55791209f2958dee1a7ef2e04873c62a2cd9
Colin But <colin.but@outlook.com>
2020-03-26 10:19:16 +0000

    adding sample orders to OrderService

---
+++ order-service/src/main/java/com/mycompany/orderservice/config/ApplicationConfig.java
@@ -0,0 +1,40 @@
+package com.mycompany.orderservice.config;
+
+import com.mycompany.orderservice.entity.Order;
+import com.mycompany.orderservice.repository.OrderRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+@Component
+public class ApplicationConfig {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ApplicationConfig.class);
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @PostConstruct
+    public void init(){
+        LOGGER.info("Populating MongoDB with some sample orders");
+        orderRepository.saveAll(Arrays.asList(
+                order("1", Arrays.asList("Pink Lady Apple", "Apple iPad Mini - 4th Gen")),
+                order("2", Arrays.asList("Fingers", "Clubs Orange", "Onions")),
+                order("2", Arrays.asList("Fingers", "Clubs Orange", "Bottle of Water Evian")),
+                order("3", Collections.singletonList("Samsung TV 42-inch Smart TV"))
+        ));
+    }
+
+    private Order order(String customerId, List<String> items){
+        Order order = new Order();
+        order.setCustomerId(customerId);
+        order.setItems(items);
+        return order;
+    }
+}
