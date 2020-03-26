415be160ccf480e7c37df7cfd356019ac123a677
Colin But <colin.but@outlook.com>
2020-03-26 10:19:46 +0000

    Adding sample items to ProductService

---
+++ product-service/src/main/java/com/mycompany/productservice/config/ApplicationConfig.java
@@ -0,0 +1,37 @@
+package com.mycompany.productservice.config;
+
+import com.mycompany.productservice.entity.Item;
+import com.mycompany.productservice.repository.ItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.math.BigDecimal;
+import java.util.Arrays;
+
+@Component
+public class ApplicationConfig {
+
+    @Autowired
+    private ItemRepository itemRepository;
+
+    @PostConstruct
+    public void init(){
+        itemRepository.saveAll(Arrays.asList(
+                item("Fingers", BigDecimal.valueOf(1.00)),
+                item("Clubs Orange", BigDecimal.valueOf(0.99)),
+                item("Onions", BigDecimal.valueOf(0.99)),
+                item("Bottle of Water Evian", BigDecimal.valueOf(1.45)),
+                item("Pink Lady Apple", BigDecimal.valueOf(2.00)),
+                item("Apple iPad Mini - 4th Gen", BigDecimal.valueOf(399.99)),
+                item("Samsung TV 42-inch Smart TV", BigDecimal.valueOf(445.00))
+        ));
+    }
+
+    private Item item(String name, BigDecimal price){
+        Item item = new Item();
+        item.setName(name);
+        item.setPrice(price);
+        return item;
+    }
+}
