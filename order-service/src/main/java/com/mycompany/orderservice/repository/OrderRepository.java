86029604ce61b3ea5dba5ae9c7ebdc5da30a040b
Colin But <colin.but@outlook.com>
2020-03-27 17:41:57 +0000

    OrderService: adding missing @Repository annotation for discovering mongodb repositories in Spring Boot although not entirely required

--- order-service/src/main/java/com/mycompany/orderservice/repository/OrderRepository.java
+++ order-service/src/main/java/com/mycompany/orderservice/repository/OrderRepository.java
@@ -2,9 +2,11 @@ package com.mycompany.orderservice.repository;
 
 import com.mycompany.orderservice.entity.Order;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface OrderRepository extends MongoRepository<Order, String> {
     List<Order> findOrderByCustomerId(String customerId);
 }
