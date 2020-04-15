0a2cbcc2ba9ae57f051c41128ef84c5dd4f89cf9
Colin But <colin.but@outlook.com>
2020-04-15 18:15:52 +0100

    OrderService: added profile support for staging & prod plus extra logging

--- order-service/src/main/java/com/mycompany/orderservice/config/ApplicationConfig.java
+++ order-service/src/main/java/com/mycompany/orderservice/config/ApplicationConfig.java
@@ -14,7 +14,7 @@ import java.util.Collections;
 import java.util.List;
 
 @Component
-@Profile({"local", "dev"})
+@Profile({"local", "dev", "staging", "prod"})
 public class ApplicationConfig {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ApplicationConfig.class);
--- order-service/src/main/java/com/mycompany/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/mycompany/orderservice/controller/OrderController.java
@@ -2,6 +2,8 @@ package com.mycompany.orderservice.controller;
 
 import com.mycompany.orderservice.entity.Order;
 import com.mycompany.orderservice.repository.OrderRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +15,8 @@ import java.util.List;
 @RestController
 public class OrderController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(OrderController.class);
+
     @Autowired
     private OrderRepository orderRepository;
 
@@ -23,12 +27,15 @@ public class OrderController {
 
     @GetMapping("/orders")
     public ResponseEntity<List<Order>> getOrdersForCustomer(@RequestParam(name = "customerId") String customerId){
+        LOGGER.info("Getting orders for customer: {}", customerId);
         List<Order> customerOrders = orderRepository.findOrderByCustomerId(customerId);
+        LOGGER.info("Fetched orders: {} for customer: {}", customerOrders, customerId);
         return ResponseEntity.ok(customerOrders);
     }
 
     @PostMapping("/order")
     public ResponseEntity createOrder(@RequestBody Order order){
+        LOGGER.info("Creating new Order: {}", order);
         orderRepository.save(order);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
